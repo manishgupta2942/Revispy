e857187e2bf32212ce4a43a8966c927571ee48d9
manishgupta2942 <139032999+manishgupta2942@users.noreply.github.com>
2024-10-27 14:53:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
       proxy: {
           '/api': {
-              target: 'http://localhost:5000', // Proxy to backend
+              target: 'https://revispy-1.onrender.com/', // Proxy to backend
               changeOrigin: true,
               secure: false,
           },
